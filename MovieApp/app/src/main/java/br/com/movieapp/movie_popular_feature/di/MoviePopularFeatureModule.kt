e642f1b086fe9ff7714387f29f28499714da0868
Victor Codonho <victor_codonho@outlook.com>
2024-12-03 12:01:38 -0300

    feat: level 22

---
+++ MovieApp/app/src/main/java/br/com/movieapp/movie_popular_feature/di/MoviePopularFeatureModule.kt
@@ -0,0 +1,37 @@
+package br.com.movieapp.movie_popular_feature.di
+
+import br.com.movieapp.core.data.remote.MovieService
+import br.com.movieapp.movie_popular_feature.data.repository.MoviePopularRepositoryImpl
+import br.com.movieapp.movie_popular_feature.data.source.MoviePopularRemoteDataSourceImpl
+import br.com.movieapp.movie_popular_feature.domain.repository.MoviePopularRepository
+import br.com.movieapp.movie_popular_feature.domain.source.MoviePopularRemoteDataSource
+import br.com.movieapp.movie_popular_feature.domain.usecase.GetPopularMoviesUseCase
+import br.com.movieapp.movie_popular_feature.domain.usecase.GetPopularMoviesUseCaseImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MoviePopularFeatureModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieDataSource(service: MovieService): MoviePopularRemoteDataSource {
+        return MoviePopularRemoteDataSourceImpl(service = service)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(remoteDataSource: MoviePopularRemoteDataSource): MoviePopularRepository {
+        return MoviePopularRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMoviesPopularUseCase(moviePopularRepository: MoviePopularRepository): GetPopularMoviesUseCase {
+        return GetPopularMoviesUseCaseImpl(repository = moviePopularRepository)
+    }
+}
